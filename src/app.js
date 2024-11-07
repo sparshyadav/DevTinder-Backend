ce3294935d9eac34654e4ce881ffec0f813e21e3
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-11-07 08:17:59 +0530

    Start Working on the Project

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+
+app.use((req, res)=>{
+    res.send("Hello from the Server");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is Successfully Listening on Port: 3000");
+});
