97544e97d8727858b702af90481580305e2eeb11
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-11-09 12:04:41 +0530

    User model Created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const connectDB = require("./config/database.js");
+const app = express();
 
-app.use("/", (req, res)=>{
+app.use("/", (req, res) => {
     res.send("Hello from the Server");
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
+    connectDB();
     console.log("Server is Successfully Listening on Port: 3000");
 });
