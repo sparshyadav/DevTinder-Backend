b1683c58e8b88ff05f532afd06035be065eaeb57
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-11-09 12:05:42 +0530

    User Model Created

--- src/models/user.js
+++ src/models/user.js
@@ -21,6 +21,4 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = userModel;
+module.exports = mongoose.model("User", userSchema);
