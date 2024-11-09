5de051d0e7f2a73e9823be8abdb808da26c4541b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-11-09 12:27:37 +0530

    API Endpoint Created to Create New User

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
+const User = require("./models/user.js")
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Hello from the Server");
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId: "vk@gmail.com",
+        password: "vk123"
+    });
+    await user.save();
+
+    res.send("User Added");
 })
 
 app.listen(3000, () => {
